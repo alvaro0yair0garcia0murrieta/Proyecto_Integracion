d5fa41761b0322c6a7273e920e7e266f42d5fdeb
MAPACHE <murrieta.alvaro@gmail.com>
2023-06-01 15:37:37 -0600

    g

--- Api empleados/src/main/java/com/airopuerto/empleados/Contrololador.java
+++ Api empleados/src/main/java/com/airopuerto/empleados/Contrololador.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/personal)
+@RequestMapping("/personal")
 public class Contrololador {
     @Autowired
         private Repositorio repositorio;
