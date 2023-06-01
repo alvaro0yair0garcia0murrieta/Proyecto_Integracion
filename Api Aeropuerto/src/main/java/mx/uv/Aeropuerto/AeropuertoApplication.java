a58b0f5b266bdf0d0a9675308a251fd434311087
David Vega Tepetla <pescador2111@hotmail.com>
2023-05-31 23:23:51 -0600

    ya sirve la api aeropuerto

--- Api Aeropuerto/src/main/java/mx/uv/Aeropuerto/AeropuertoApplication.java
+++ Api Aeropuerto/src/main/java/mx/uv/Aeropuerto/AeropuertoApplication.java
@@ -1,16 +1,25 @@
 package mx.uv.Aeropuerto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @SpringBootApplication
 public class AeropuertoApplication {
-
+	@Autowired
+	private Repositorio repositorio;
 	public static void main(String[] args) {
 		SpringApplication.run(AeropuertoApplication.class, args);
 	}
@@ -21,9 +30,36 @@ public class AeropuertoApplication {
 	}
 
 	@GetMapping(value="/aeropuerto")
-	public String getMethodName(@RequestParam String param) {
-		return "aeropuerto 1";
+	public List<Aeropuerto> getMethodName() {
+		Iterable<Aeropuerto> aeropuertos = repositorio.findAll();
+   		List<Aeropuerto> aeropuertosList = new ArrayList<>();
+   		aeropuertos.forEach(aeropuertosList::add); 
+		return aeropuertosList;
 	}
-
-
+	@GetMapping(value = "/aeropuerto/{nombre}")
+	public Aeropuerto getAeropuertoByNombre(@PathVariable String nombre){
+		Aeropuerto aeropuerto = repositorio.findByNombre(nombre);
+		return aeropuerto;
+	}
+	//no se pueden poner nombres iguales porque crachea la api
+	@PostMapping(value = "/aeropuerto")
+	public Aeropuerto postAeropuerto(@RequestBody Aeropuerto aeropuerto){
+		repositorio.save(aeropuerto);
+		return aeropuerto;
+	}
+	@DeleteMapping(value = "/aeropuerto/{nombre}")
+	public Aeropuerto deleteAeropuertoByNombre(@PathVariable String nombre){
+		Aeropuerto aeropuerto = repositorio.findByNombre(nombre);
+		repositorio.delete(aeropuerto);
+		return aeropuerto;
+	}
+	@PatchMapping(value = "/aeropuerto/{nombre}")
+	public Aeropuerto PatchAeropuerto(@PathVariable String nombre,@RequestBody Aeropuerto aeropuerto){
+		Aeropuerto aeroTemp = repositorio.findByNombre(nombre);
+		aeroTemp.setDescripcion(aeropuerto.getDescripcion());
+		aeroTemp.setNombre(aeropuerto.getNombre());
+		aeroTemp.setUbicacion(aeropuerto.getUbicacion());
+		aeroTemp = repositorio.save(aeroTemp);
+		return aeroTemp;
+	}
 }
